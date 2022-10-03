7f8675e3fa4c50543416e7b2393419757dabfeca
Pradhyankita Bhasme <pradhyankita@gmail.com>
2022-10-03 14:45:56 +0530

    Adding: generics-class-with-3-variables

---
+++ src/com/bridgelabz/Gen/FindMaximum.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.Gen;
+
+public class FindMaximum<K extends Comparable> {
+
+        K getMaximum(K n1, K n2, K n3){
+            K max=n1;
+            if (n2.compareTo(max) > 0) max=n2;
+            if (n3.compareTo(max) > 0) max=n3;
+            return max;
+        }
+}
